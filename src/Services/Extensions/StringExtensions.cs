4fa9c01f7e72052c1bc6fe413b8aadc56973c92a
Vitalii Savitskii <visa@ecco.com>
2024-04-17 11:09:03 +0200

    Add StreamConfigurationProvider

---
+++ src/Services/Extensions/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arcane.Framework.Services.Extensions;
+
+/// <summary>
+/// Extension methods for string objects
+/// </summary>
+public static class StringExtensions
+{
+    private static string DefaultVarPrefix => $"{nameof(Arcane).ToUpper()}__";
+
+    /// <summary>
+    /// Returns the environment variable name
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string GetEnvironmentVariableName(this string name) => $"{DefaultVarPrefix}{name}".ToUpperInvariant();
+
+    /// <summary>
+    /// Returns the environment variable value
+    /// </summary>
+    /// <param name="name">Environment variable name</param>
+    /// <returns>Value as a string</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the variable is not present in environment</exception>
+    public static string GetEnvironmentVariable(this string name)
+        => Environment.GetEnvironmentVariable(GetEnvironmentVariableName(name)) ??
+           throw new ArgumentNullException(name);
+}
---
+++ src/Services/StreamConfigurationProvider.cs
@@ -0,0 +1,24 @@
+﻿using Arcane.Framework.Services.Base;
+using Arcane.Framework.Services.Extensions;
+
+namespace Arcane.Framework.Services;
+
+/// <summary>
+/// Provides access to a streaming job configuration
+/// </summary>
+public class StreamConfigurationProvider: IStreamConfigurationProvider
+{
+    /// <summary>
+    /// Stream kind environment variable name
+    /// </summary>
+    private const string STREAM_KIND = nameof(STREAM_KIND);
+
+    /// <summary>
+    /// Returns stream kind from environment
+    /// </summary>
+    /// <returns></returns>
+    public string GetStreamKind()
+    {
+        return STREAM_KIND.GetEnvironmentVariable();
+    }
+}
