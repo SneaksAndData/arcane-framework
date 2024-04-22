99109e3a87203193978505ab2f6882207aa9072b
Vitalii Savitskii <visa@ecco.com>
2024-04-22 15:23:45 +0200

    Add assembly environment variables

--- src/Services/Base/IStreamOptions.cs
+++ src/Services/Base/IStreamOptions.cs
@@ -1,22 +1,23 @@
 ﻿namespace Arcane.Framework.Services.Base;
 
 /// <summary>
-/// Provides red-only access to the stream configuration properties and the stream metadata to the stream stages.
+/// Read-write stream context options used by the application configuration pipeline
+/// This interface should be implemented by the streaming plugin.
 /// </summary>
-public interface IStreamContext
+public interface IStreamOptions: IStreamContext
 {
     /// <summary>
     /// Id of the stream
     /// </summary>
-    public string StreamId { get; }
+    public string StreamId { get; set; }
 
     /// <summary>
     /// True if stream is running in backfill (full reload) mode
     /// </summary>
-    public bool IsRunningInBackfillMode { get; }
+    public bool IsRunningInBackfillMode { get; set; }
 
     /// <summary>
     /// Kind of the custom resource that manages the stream
     /// </summary>
-    public string StreamKind { get; }
+    public string StreamKind{ get; set;  }
 }
