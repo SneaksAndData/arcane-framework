3769e01d20ca7cbf70c91be079b123f95f12a2c1
Vitalii Savitskii <visa@ecco.com>
2024-04-22 15:25:08 +0200

    Build fix

--- src/Providers/StreamContext/StreamContextProvider.cs
+++ src/Providers/StreamContext/StreamContextProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
-using Arcane.Framework.Extensions;
 using Arcane.Framework.Services.Base;
 using Microsoft.Extensions.DependencyInjection;
 using Snd.Sdk.Hosting;
