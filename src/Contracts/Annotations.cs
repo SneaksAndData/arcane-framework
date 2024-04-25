1d87dd0e407dc8586206931d67375e94749f0827
Vitalii Savitskii <visa@ecco.com>
2024-04-25 15:12:05 +0200

    Add StreamStatusService class and IStreamGraphBuilder interface

---
+++ src/Contracts/Annotations.cs
@@ -0,0 +1,22 @@
+﻿namespace Arcane.Framework.Contracts;
+
+/// <summary>
+/// Streaming job annotation keys that can be used by a StreamRunner and a Stream Operator
+/// This class should be removed in future and replaced with Stream-class based contracts, see #23
+/// </summary>
+public class Annotations
+{
+    /// <summary>
+    /// Annotation that signals to StreamOperator to start backfill process
+    /// If the streaming job was marked with this annotation, StreamOperator stops the current stream and starts
+    /// a backfill job.
+    /// </summary>
+    public const string STATE_ANNOTATION_KEY = "arcane.streaming.sneaksanddata.com/state";
+
+    /// <summary>
+    /// Annotation value signals to StreamOperator to start backfill process
+    /// If the streaming job was marked with this annotation, StreamOperator stops the current stream and starts
+    /// a backfill job.
+    /// </summary>
+    public const string SCHEMA_MISMATCH_STATE_ANNOTATION_VALUE = "schema-mismatch";
+}
---
+++ src/Services/Base/IStreamGraphBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Akka.Streams;
+using Akka.Streams.Dsl;
+
+namespace Arcane.Framework.Services.Base;
+
+/// <summary>
+/// Base interface for classes that build Akka.NET stream graphs.
+/// This interface is intended to be implemented by the streaming plugin.
+/// </summary>
+/// <typeparam name="TStreamContext">Type of the StreamContext class</typeparam>
+public interface IStreamGraphBuilder<in TStreamContext> where TStreamContext : IStreamContext
+{
+    /// <summary>
+    /// Build an instance of the runnable Akka.NET graph.
+    /// </summary>
+    /// <param name="context">StreamContext instance.</param>
+    /// <returns>Runnable graph that materialized to a UniqueKillSwitch and a Task.</returns>
+    public IRunnableGraph<(UniqueKillSwitch, Task)> BuildGraph(TStreamContext context);
+}
