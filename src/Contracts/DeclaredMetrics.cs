6ac52a6d64922469db6100486b6f89be2af06699
Vitalii Savitskii <visa@ecco.com>
2024-04-25 17:38:49 +0200

    Add the RunStream method

---
+++ src/Contracts/DeclaredMetrics.cs
@@ -0,0 +1,12 @@
+﻿namespace Arcane.Framework.Contracts;
+
+/// <summary>
+/// Metrics published by Arcane stream plugin
+/// </summary>
+public static class DeclaredMetrics
+{
+    /// <summary>
+    /// Number of data rows processed by streamer
+    /// </summary>
+    public const string ROWS_INCOMING = "rows.incoming";
+}
---
+++ src/Services/StreamExceptionHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Util;
+using Akka.Util.Extensions;
+using Arcane.Framework.Contracts;
+using Arcane.Framework.Services.Base;
+using Arcane.Framework.Sources.Exceptions;
+using Microsoft.Extensions.Logging;
+using Snd.Sdk.Tasks;
+
+namespace Arcane.Framework.Services;
+
+internal class StreamExceptionHandler: IArcaneExceptionHandler
+{
+    private readonly ILogger<StreamExceptionHandler> logger;
+    private readonly IStreamStatusService streamStatusService;
+    private readonly IStreamContext streamContext;
+
+    public StreamExceptionHandler(ILogger<StreamExceptionHandler> logger,
+        IStreamStatusService streamStatusService, IStreamContext streamContext)
+    {
+        this.logger = logger;
+        this.streamStatusService = streamStatusService;
+        this.streamContext = streamContext;
+    }
+
+    private Task<Option<int>> HandleSchemaMismatch()
+    {
+        this.logger.LogInformation("Schema mismatch detected. Reporting schema mismatch and exiting");
+        return this.streamStatusService.ReportSchemaMismatch(this.streamContext.StreamId).Map(_ => ExitCodes.SUCCESS.AsOption());
+    }
+
+    private Task<Option<int>> HandleSchemaInconsistency()
+    {
+        this.logger.LogInformation("Schema mismatch detected. Reporting schema mismatch and exiting");
+        return Task.FromResult(ExitCodes.RESTART.AsOption());
+    }
+
+    public Task<Option<int>> HandleException(Exception exception)
+    {
+        return exception switch
+        {
+            SchemaMismatchException => this.HandleSchemaMismatch(),
+            SchemaInconsistentException => this.HandleSchemaInconsistency(),
+            _ => Task.FromResult(Option<int>.None)
+        };
+    }
+}
