3252fa67acf72932f20b60d5c270568d718432aa
Vitalii Savitskii <visa@ecco.com>
2024-05-28 12:58:23 +0200

    Rollback state annotation changes

--- src/Contracts/Annotations.cs
+++ src/Contracts/Annotations.cs
@@ -11,7 +11,7 @@ public class Annotations
     /// If the streaming job was marked with this annotation, StreamOperator stops the current stream and starts
     /// a backfill job.
     /// </summary>
-    public const string STATE_ANNOTATION_KEY = "arcane.streaming.sneaksanddata.com/state";
+    public const string STATE_ANNOTATION_KEY = "arcane/state";
 
     /// <summary>
     /// Annotation value signals to StreamOperator to start backfill process
