202ab12e3a2b2bdb37e76944de6578560097d276
Vitalii Savitskii <visa@ecco.com>
2024-07-17 13:55:37 +0200

    Allow using custom properties in Arcane.Framework (#83)
    This PR adds generalized version of https://github.com/SneaksAndData/arcane-stream-blob-storage/pull/26
    
    This is required for uniformed logging in Arcane streams

---
+++ src/Contracts/Logging.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Serilog;
+using SnD.Sdk.Extensions.Environment.Hosting;
+
+namespace Arcane.Framework.Contracts;
+
+/// <summary>
+/// Logging extension methods for Arcane framework.
+/// </summary>
+public static class Logging
+{
+    /// <summary>
+    /// Allows enriching the logger with properties from the environment.
+    /// </summary>
+    /// <param name="configuration"></param>
+    /// <returns></returns>
+    public static LoggerConfiguration EnrichWithCustomProperties(this LoggerConfiguration configuration)
+    {
+        var loggingProperties = EnvironmentExtensions.GetDomainEnvironmentVariable("LOGGING_PROPERTIES");
+        if (string.IsNullOrEmpty(loggingProperties))
+        {
+            return configuration;
+        }
+
+        var properties = JsonSerializer.Deserialize<Dictionary<string, string>>(loggingProperties);
+        foreach (var (key, value) in properties)
+        {
+            configuration.Enrich.WithProperty(key, value);
+        }
+
+        return configuration;
+    }
+}
