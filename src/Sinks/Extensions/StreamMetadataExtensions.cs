d7493e2c7f9e985eb781c43cfa77dca4a810bb73
Vitalii Savitskii <visa@ecco.com>
2024-10-08 14:21:28 +0200

    Add docstrings

--- src/Sinks/Extensions/StreamMetadataExtensions.cs
+++ src/Sinks/Extensions/StreamMetadataExtensions.cs
@@ -1,4 +1,3 @@
-using Akka.Event;
 using Arcane.Framework.Sinks.Models;
 using Arcane.Framework.Sinks.Services;
 using Arcane.Framework.Sinks.Services.Base;
@@ -6,8 +5,20 @@ using Snd.Sdk.Storage.Base;
 
 namespace Arcane.Framework.Sinks.Extensions;
 
+/// <summary>
+/// Extension methods for StreamMetadata class
+/// </summary>
 public static class StreamMetadataExtensions
 {
+    /// <summary>
+    /// Creates a new IMetadataWriter instance from the StreamMetadata instance
+    /// </summary>
+    /// <param name="streamMetadata">The object that hods the stream metadata</param>
+    /// <param name="writer">Blob Storage writer to be used to perform the write operation.</param>
+    /// <param name="basePath">The Sink root directory</param>
+    /// <returns>
+    /// Metadata writer instance that can write one or more metadata fields to the given root directory.
+    /// </returns>
    public static IMetadataWriter ToStreamMetadataWriter(this StreamMetadata streamMetadata,
        IBlobStorageWriter writer,
        string basePath)
